920a430775d974fd7531ffbb7fb81a90416d63b4
Andrey V.Kosteltsev <kx@radix.pro>
2014-03-18 15:13:57 +0000

    gcc cross building for bcm74x
    git-svn-id: https://svn.radix.pro/svn/platform/trunk@44 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/GNU/diffutils/create-3.2-mkdir-p-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=3.2
+
+tar --files-from=file.list -xJf ../diffutils-$VERSION.tar.xz
+mv diffutils-$VERSION diffutils-$VERSION-orig
+
+cp -rf ./diffutils-$VERSION-new ./diffutils-$VERSION
+
+diff -b --unified -Nr  diffutils-$VERSION-orig  diffutils-$VERSION > diffutils-$VERSION-mkdir-p.patch
+
+mv diffutils-$VERSION-mkdir-p.patch ../patches
+
+rm -rf ./diffutils-$VERSION
+rm -rf ./diffutils-$VERSION-orig
---
+++ sources/GNU/diffutils/create-3.3-waitpid-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=3.3
+
+tar --files-from=file.list -xJf ../diffutils-$VERSION.tar.xz
+mv diffutils-$VERSION diffutils-$VERSION-orig
+
+cp -rf ./diffutils-$VERSION-new ./diffutils-$VERSION
+
+diff -b --unified -Nr  diffutils-$VERSION-orig  diffutils-$VERSION > diffutils-$VERSION-waitpid.patch
+
+mv diffutils-$VERSION-waitpid.patch ../patches
+
+rm -rf ./diffutils-$VERSION
+rm -rf ./diffutils-$VERSION-orig

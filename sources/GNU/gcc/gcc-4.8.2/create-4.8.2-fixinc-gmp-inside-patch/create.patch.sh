585724195add5e7d7960530ed7ccfc77621cc996
Andrey V.Kosteltsev <kx@radix.pro>
2014-03-19 10:42:02 +0000

    GCC for target with gmp/mpfr/mpc inside of src tree
    git-svn-id: https://svn.radix.pro/svn/platform/trunk@45 0a1960e2-aef6-4eae-af04-ff5de3100f3e

--- sources/GNU/gcc/gcc-4.8.2/create-4.8.2-fixinc-gmp-inside-patch/create.patch.sh
+++ sources/GNU/gcc/gcc-4.8.2/create-4.8.2-fixinc-gmp-inside-patch/create.patch.sh
@@ -7,9 +7,9 @@ mv gcc-$VERSION gcc-$VERSION-orig
 
 cp -rf ./gcc-$VERSION-new ./gcc-$VERSION
 
-diff -b --unified -Nr  gcc-$VERSION-orig  gcc-$VERSION > gcc-$VERSION-fixincludes.patch
+diff -b --unified -Nr  gcc-$VERSION-orig  gcc-$VERSION > gcc-$VERSION-fixinc-gmp-inside.patch
 
-mv gcc-$VERSION-fixincludes.patch ../patches
+mv gcc-$VERSION-fixinc-gmp-inside.patch ../patches
 
 rm -rf ./gcc-$VERSION
 rm -rf ./gcc-$VERSION-orig

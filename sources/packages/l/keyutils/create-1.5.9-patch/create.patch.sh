6cc1fb2a05a6dc1cb776fcb586b87682deb07af7
Andrey V.Kosteltsev <kx@radix.pro>
2014-07-14 11:48:13 +0000

    Linux Key Management Utilities
    git-svn-id: https://svn.radix.pro/svn/platform/trunk@77 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/packages/l/keyutils/create-1.5.9-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=1.5.9
+
+tar --files-from=file.list -xjvf ../keyutils-$VERSION.tar.bz2
+mv keyutils-$VERSION keyutils-$VERSION-orig
+
+cp -rf ./keyutils-$VERSION-new ./keyutils-$VERSION
+
+diff -b --unified -Nr  keyutils-$VERSION-orig  keyutils-$VERSION > keyutils-$VERSION.patch
+
+mv keyutils-$VERSION.patch ../patches
+
+rm -rf ./keyutils-$VERSION
+rm -rf ./keyutils-$VERSION-orig

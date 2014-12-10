f4c06386f8a23444fb9318a67e82a07432afba90
Andrey V.Kosteltsev <kx@radix.pro>
2014-12-10 15:00:50 +0000

    X.org font collection
    git-svn-id: https://svn.radix.pro/svn/platform/trunk@138 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ X11/X.org/font/font-misc-meltho/1.0.3/font-misc-meltho-pkg-install.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+# Preserve new files
+install_file() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+
+# arg 1:  the new package version
+pre_install() {
+  /bin/true
+}
+
+# arg 1:  the new package version
+post_install() {
+  # We use an relative path to 'proc/sys/kernel/osrelease' because we have
+  # to be sure that we are running on the target platform. Only in this case
+  # we will use absolute path to mkfonts{dir,scale}.
+  if [ -r proc/sys/kernel/osrelease -a -x /usr/bin/mkfontdir -a -x /usr/bin/mkfontscale ]; then
+    ( cd /usr/share/fonts/OTF
+      mkfontscale .
+      mkfontdir .
+    )
+  fi
+  # We use an relative path to 'proc/sys/kernel/osrelease' because we have
+  # to be sure that we are running on the target platform. Only in this case
+  # we will use absolute path to fc-cache.
+  if [ -r proc/sys/kernel/osrelease -a -x /usr/bin/fc-cache ]; then
+    /usr/bin/fc-cache -f
+  fi
+}
+
+# arg 1:  the new package version
+# arg 2:  the old package version
+pre_upgrade() {
+  /bin/true
+}
+
+# arg 1:  the new package version
+# arg 2:  the old package version
+post_upgrade() {
+  post_install
+}
+
+# arg 1:  the old package version
+pre_remove() {
+  /bin/true
+}
+
+# arg 1:  the old package version
+post_remove() {
+  /bin/true
+}
+
+
+operation=$1
+shift
+
+$operation $*

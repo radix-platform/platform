1d994dc88c8f7ca7b3d858ce4712ee8bb42e923c
Andrey V.Kosteltsev <kx@radix.pro>
2014-12-10 12:06:20 +0000

    Xdm-1.1.11
    git-svn-id: https://svn.radix.pro/svn/platform/trunk@137 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ X11/X.org/app/xdm/1.1.11/xdm-pkg-install.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Preserve new files
+install_file() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+
+# arg 1:  the new package version
+pre_install() {
+  /bin/true
+}
+
+# arg 1:  the new package version
+post_install() {
+  for conffile in Xaccess Xreset Xresources Xservers Xsession Xsetup_0 Xstartup Xwilling \
+                  GiveConsole TakeConsole xdm-config ../app-defaults/Chooser ; do
+    install_file etc/X11/xdm/${conffile}.new
+  done
+}
+
+# arg 1:  the new package version
+# arg 2:  the old package version
+pre_upgrade() {
+  /bin/true
+}
+
+# arg 1:  the new package version
+# arg 2:  the old package version
+post_upgrade() {
+  post_install
+}
+
+# arg 1:  the old package version
+pre_remove() {
+  /bin/true
+}
+
+# arg 1:  the old package version
+post_remove() {
+  /bin/true
+}
+
+
+operation=$1
+shift
+
+$operation $*
---
+++ sources/X.org/app/xdm/create-1.1.11-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=1.1.11
+
+tar --files-from=file.list -xjvf ../xdm-$VERSION.tar.bz2
+mv xdm-$VERSION xdm-$VERSION-orig
+
+cp -rf ./xdm-$VERSION-new ./xdm-$VERSION
+
+diff -b --unified -Nr  xdm-$VERSION-orig  xdm-$VERSION > xdm-$VERSION.patch
+
+mv xdm-$VERSION.patch ../patches
+
+rm -rf ./xdm-$VERSION
+rm -rf ./xdm-$VERSION-orig

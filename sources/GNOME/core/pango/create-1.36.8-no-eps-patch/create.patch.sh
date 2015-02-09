368d5008cde946b7bde1c9472dc791379f65ec28
Andrey V.Kosteltsev <kx@radix.pro>
2015-02-09 08:25:25 +0000

    special libs/pango patch for skipping cairo SVG,PDF,PS features; fix icu4c LN_SF
    git-svn-id: https://svn.radix.pro/svn/platform/trunk@151 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/GNOME/core/pango/create-1.36.8-no-eps-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=1.36.8
+
+tar --files-from=file.list -xJvf ../pango-$VERSION.tar.xz
+mv pango-$VERSION pango-$VERSION-orig
+
+cp -rf ./pango-$VERSION-new ./pango-$VERSION
+
+diff -b --unified -Nr  pango-$VERSION-orig  pango-$VERSION > pango-$VERSION-no-eps.patch
+
+mv pango-$VERSION-no-eps.patch ../patches
+
+rm -rf ./pango-$VERSION
+rm -rf ./pango-$VERSION-orig

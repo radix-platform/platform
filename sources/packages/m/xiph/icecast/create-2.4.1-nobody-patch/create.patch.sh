be4dc82b5ced49daf2aaa2f3c6c0b03f0e5291cc
Andrey V.Kosteltsev <kx@radix.pro>
2015-02-18 11:29:24 +0000

    speex, icecast, libshout
    git-svn-id: https://svn.radix.pro/svn/platform/trunk@165 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/packages/m/xiph/icecast/create-2.4.1-nobody-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=2.4.1
+
+tar --files-from=file.list -xzvf ../icecast-$VERSION.tar.gz
+mv icecast-$VERSION icecast-$VERSION-orig
+
+cp -rf ./icecast-$VERSION-new ./icecast-$VERSION
+
+diff -b --unified -Nr  icecast-$VERSION-orig  icecast-$VERSION > icecast-$VERSION-nobody.patch
+
+mv icecast-$VERSION-nobody.patch ../patches
+
+rm -rf ./icecast-$VERSION
+rm -rf ./icecast-$VERSION-orig

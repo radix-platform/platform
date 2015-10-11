9dd8f60ddd6a29d29a849c9348e359c64a7ecaab
Andrey V.Kosteltsev <kx@radix.pro>
2015-10-11 17:41:23 +0000

    added .dialogrc to the root home directory
    git-svn-id: https://svn.radix.pro/svn/platform/branches/genext4fs@242 0a1960e2-aef6-4eae-af04-ff5de3100f3e

--- base/pkgtool/pkg-install.sh
+++ base/pkgtool/pkg-install.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# Preserve new files
+install_file() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+
 # arg 1:  the new package version
 pre_install() {
   /bin/true
@@ -7,7 +21,7 @@ pre_install() {
 
 # arg 1:  the new package version
 post_install() {
-  /bin/true
+  install_file root/.dialogrc.new
 }
 
 # arg 1:  the new package version

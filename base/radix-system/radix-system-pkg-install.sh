f9639e671ccf6b58c9146bcc75deba660d03f3c0
Andrey V.Kosteltsev <kx@radix.pro>
2015-10-21 12:37:01 +0000

    Set Xorg suid bit to be able to start X by nonprivileged user.
    Enabled udev daemon for omap5-uevm board.
    
    Additional inodes for installation in the init-devices package.
    
    System init scripts and tart/stop daemons package named radix-system.
    
    A10, A20, omap543x, and ci20 boot loaders now create the unallocated space record which contains u-boot-spl.bin and u-boot.img to simplify the target disk or SD card preparation.
    
    Also during the omap543x u-boot building we create FAT32 boot file system image which required for strange omap u-boot.
    
    
    
    git-svn-id: https://svn.radix.pro/svn/platform/branches/genext4fs@246 0a1960e2-aef6-4eae-af04-ff5de3100f3e

--- base/radix-system/radix-system-pkg-install.sh
+++ base/radix-system/radix-system-pkg-install.sh
@@ -21,7 +21,54 @@ pre_install() {
 
 # arg 1:  the new package version
 post_install() {
-  /bin/true
+  install_file etc/HOSTNAME.new
+  install_file etc/csh.login.new
+  install_file etc/filesystems.new
+  install_file etc/fstab.new
+  install_file etc/hosts.new
+  install_file etc/group.new
+  install_file etc/gshadow.new
+  install_file etc/inittab.new
+  install_file etc/inputrc.new
+  install_file etc/issue.new
+  install_file etc/issue.net.new
+  install_file etc/networks.new
+  install_file etc/nsswitch.conf.new
+  install_file etc/passwd.new
+  install_file etc/printcap.new
+  install_file etc/profile.new
+  install_file etc/securetty.new
+  install_file etc/services.new
+  install_file etc/shadow.new
+  install_file etc/shells.new
+  install_file etc/termcap.new
+
+  install_file etc/profile.d/lang.csh.new
+  install_file etc/profile.d/lang.sh.new
+
+  install_file etc/rc.d/rc.4.new
+  install_file etc/rc.d/rc.6.new
+  install_file etc/rc.d/rc.K.new
+  install_file etc/rc.d/rc.M.new
+  install_file etc/rc.d/rc.S.new
+
+  install_file etc/rc.d/rc.local.new
+  install_file etc/rc.d/rc.local_shutdown.new
+  install_file etc/rc.d/rc.loop.new
+  install_file etc/rc.d/rc.sysvinit.new
+
+  install_file var/log/lastlog.new
+
+  install_file etc/ld.so.conf.new
+
+  # not fully finished stuff - depends on app/kbd, app/gpm, and kernel modules:
+  install_file etc/rc.d/rc.font.new
+  install_file etc/rc.d/rc.gpm.new
+  install_file etc/rc.d/rc.keymap.new
+  install_file etc/rc.d/rc.modules.new
+
+  ( cd etc/rc.d ; rm -rf rc.0 )
+  ( cd etc/rc.d ; ln -sf rc.6 rc.0 )
 }
 
 # arg 1:  the new package version

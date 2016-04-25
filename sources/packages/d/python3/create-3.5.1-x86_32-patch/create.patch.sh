7b7baabbee414c1b4543164fbc671d1dc4197963
Andrey V.Kosteltsev <kx@radix.pro>
2016-04-25 00:59:01 +0000

    python 3.5.1 pre
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@407 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/packages/d/python3/create-3.5.1-x86_32-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=3.5.1
+
+tar --files-from=file.list -xJvf ../Python-$VERSION.tar.xz
+mv Python-$VERSION Python-$VERSION-orig
+
+cp -rf ./Python-$VERSION-new ./Python-$VERSION
+
+diff -b --unified -Nr  Python-$VERSION-orig  Python-$VERSION > Python-$VERSION-x86_32.patch
+
+mv Python-$VERSION-x86_32.patch ../patches
+
+rm -rf ./Python-$VERSION
+rm -rf ./Python-$VERSION-orig

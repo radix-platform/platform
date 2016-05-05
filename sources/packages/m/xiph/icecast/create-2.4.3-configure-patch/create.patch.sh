971028e2aa9744715baa86f7b9131840c4a87deb
Andrey V.Kosteltsev <kx@radix.pro>
2016-05-05 07:59:18 +0000

    icecast 2.4.3
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@414 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/packages/m/xiph/icecast/create-2.4.3-configure-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=2.4.3
+
+tar --files-from=file.list -xzvf ../icecast-$VERSION.tar.gz
+mv icecast-$VERSION icecast-$VERSION-orig
+
+cp -rf ./icecast-$VERSION-new ./icecast-$VERSION
+
+diff -b --unified -Nr  icecast-$VERSION-orig  icecast-$VERSION > icecast-$VERSION-configure.patch
+
+mv icecast-$VERSION-configure.patch ../patches
+
+rm -rf ./icecast-$VERSION
+rm -rf ./icecast-$VERSION-orig

151af73d9cd6a4ff74438b78ad2dd38db6c5dede
Andrey V.Kosteltsev <kx@radix.pro>
2016-05-17 06:58:41 +0000

    freetype 2.6.3 sources
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@448 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/packages/l/freetype/create-2.6.3-subpixel-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=2.6.3
+
+tar --files-from=file.list -xjvf ../freetype-$VERSION.tar.bz2
+mv freetype-$VERSION freetype-$VERSION-orig
+
+cp -rf ./freetype-$VERSION-new ./freetype-$VERSION
+
+diff -b --unified -Nr  freetype-$VERSION-orig  freetype-$VERSION > freetype-$VERSION-subpixel.patch
+
+mv freetype-$VERSION-subpixel.patch ../patches
+
+rm -rf ./freetype-$VERSION
+rm -rf ./freetype-$VERSION-orig

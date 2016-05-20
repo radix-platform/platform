5e792461a3e70b293e99976a69b47f0317ebca0a
Andrey V.Kosteltsev <kx@radix.pro>
2016-05-20 08:06:58 +0000

    X.org application sources update
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@461 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/X.org/app/xcompmgr/create-1.1.7-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=1.1.7
+
+tar --files-from=file.list -xjvf ../xcompmgr-$VERSION.tar.bz2
+mv xcompmgr-$VERSION xcompmgr-$VERSION-orig
+
+cp -rf ./xcompmgr-$VERSION-new ./xcompmgr-$VERSION
+
+diff -b --unified -Nr  xcompmgr-$VERSION-orig  xcompmgr-$VERSION > xcompmgr-$VERSION.patch
+
+mv xcompmgr-$VERSION.patch ../patches
+
+rm -rf ./xcompmgr-$VERSION
+rm -rf ./xcompmgr-$VERSION-orig

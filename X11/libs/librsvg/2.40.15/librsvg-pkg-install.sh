030ce4b9d29442718dcacd579f80a3e6e4ce3fec
Andrey V.Kosteltsev <kx@radix.pro>
2016-06-10 13:20:39 +0000

    libcroco-0.6.11, librsvg-2.40.15
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@483 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ X11/libs/librsvg/2.40.15/librsvg-pkg-install.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Preserve new files
+install_file() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+
+# arg 1:  the new package version
+pre_install() {
+  /bin/true
+}
+
+# arg 1:  the new package version
+post_install() {
+  # Notice we use an absolute path below, rather than usr/bin/update-gdk-pixbuf-loaders.
+  # This is because we're testing to see if we are on the bootdisk, which will not have
+  # /usr/bin/update-gdk-pixbuf-loaders.
+  # Also we have to check that we are not in the installer mode on the target system
+  # ("/etc/system-installer"), and we have to be sure that we are on the working system
+  # on the target hardware ("proc/sys/kernel/osrelease" - relative path).
+  if [ -r proc/sys/kernel/osrelease -a ! -r /etc/system-installer -a -x /usr/bin/update-gdk-pixbuf-loaders ]; then
+    /usr/bin/update-gdk-pixbuf-loaders
+  fi
+}
+
+# arg 1:  the new package version
+# arg 2:  the old package version
+pre_update() {
+  /bin/true
+}
+
+# arg 1:  the new package version
+# arg 2:  the old package version
+post_update() {
+  post_install
+}
+
+# arg 1:  the old package version
+pre_remove() {
+  /bin/true
+}
+
+# arg 1:  the old package version
+post_remove() {
+  /bin/true
+}
+
+
+operation=$1
+shift
+
+$operation $*

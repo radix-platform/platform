24f1c8e4bce5265d534831a4a97140d4c155a10a
Andrey V.Kosteltsev <kx@radix.pro>
2016-06-10 12:13:28 +0000

    ghostscript-9.19, qpdf-6.0.0, gdk-pixbuf2-2.35.1
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@482 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/packages/a/ghostscript/create-9.19-cross-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=9.19
+
+tar --files-from=file.list -xzvf ../ghostscript-$VERSION.tar.gz
+mv ghostscript-$VERSION ghostscript-$VERSION-orig
+
+cp -rf ./ghostscript-$VERSION-new ./ghostscript-$VERSION
+
+diff -b --unified -Nr  ghostscript-$VERSION-orig  ghostscript-$VERSION > ghostscript-$VERSION-cross.patch
+
+mv ghostscript-$VERSION-cross.patch ../patches
+
+rm -rf ./ghostscript-$VERSION
+rm -rf ./ghostscript-$VERSION-orig

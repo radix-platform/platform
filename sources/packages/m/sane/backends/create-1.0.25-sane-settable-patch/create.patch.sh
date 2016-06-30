7824807355f6963fb8203672b2f402f7048feb1b
Andrey V.Kosteltsev <kx@radix.pro>
2016-06-30 16:30:50 +0000

    sane-backends-1.0.25
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@500 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/packages/m/sane/backends/create-1.0.25-sane-settable-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=1.0.25
+
+tar --files-from=file.list -xjvf ../sane-backends-$VERSION.tar.bz2
+mv sane-backends-$VERSION sane-backends-$VERSION-orig
+
+cp -rf ./sane-backends-$VERSION-new ./sane-backends-$VERSION
+
+diff -b --unified -Nr  sane-backends-$VERSION-orig  sane-backends-$VERSION > sane-backends-$VERSION-sane-settable.patch
+
+mv sane-backends-$VERSION-sane-settable.patch ../patches
+
+rm -rf ./sane-backends-$VERSION
+rm -rf ./sane-backends-$VERSION-orig

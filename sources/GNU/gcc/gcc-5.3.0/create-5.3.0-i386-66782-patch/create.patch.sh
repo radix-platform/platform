7b7827a7db176f628fbb847d6789739a066c8d5a
Andrey V.Kosteltsev <kx@radix.pro>
2016-07-11 12:47:11 +0000

    binutils: set of patches; GCC: enable libstdc++ dual ABI, set default ABI = gcc4 compatible
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@513 0a1960e2-aef6-4eae-af04-ff5de3100f3e

--- sources/GNU/gcc/gcc-5.3.0/create-5.3.0-i386-66782-patch/create.patch.sh
+++ sources/GNU/gcc/gcc-5.3.0/create-5.3.0-i386-66782-patch/create.patch.sh
@@ -7,9 +7,9 @@ mv gcc-$VERSION gcc-$VERSION-orig
 
 cp -rf ./gcc-$VERSION-new ./gcc-$VERSION
 
-diff -b --unified -Nr  gcc-$VERSION-orig  gcc-$VERSION > gcc-$VERSION-66782.patch
+diff -b --unified -Nr  gcc-$VERSION-orig  gcc-$VERSION > gcc-$VERSION-i386-66782.patch
 
-mv gcc-$VERSION-66782.patch ../patches
+mv gcc-$VERSION-i386-66782.patch ../patches
 
 rm -rf ./gcc-$VERSION
 rm -rf ./gcc-$VERSION-orig

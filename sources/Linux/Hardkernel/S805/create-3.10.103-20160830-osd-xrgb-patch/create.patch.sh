629f833e693c28e2a6c764a5c000d464a6c6220e
Andrey V.Kosteltsev <kx@radix.pro>
2016-09-14 13:39:28 +0000

    Hardkernel-3.10.103: First step of OSD XRGB color mode backport from amlogic-3.14.29
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@532 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/Linux/Hardkernel/S805/create-3.10.103-20160830-osd-xrgb-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=3.10.103-20160830
+
+tar --files-from=file.list -xJvf ../linux-aml-$VERSION.tar.xz
+mv linux-aml-$VERSION linux-aml-$VERSION-orig
+
+cp -rf ./linux-aml-$VERSION-new ./linux-aml-$VERSION
+
+diff -b --unified -Nr  linux-aml-$VERSION-orig  linux-aml-$VERSION > linux-aml-$VERSION-osd-xrgb.patch
+
+mv linux-aml-$VERSION-osd-xrgb.patch ../patches
+
+rm -rf ./linux-aml-$VERSION
+rm -rf ./linux-aml-$VERSION-orig

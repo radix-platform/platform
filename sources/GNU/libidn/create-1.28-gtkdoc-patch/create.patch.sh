e7415a4a30fee250de1ec55783b76717a43e001d
Andrey V.Kosteltsev <kx@radix.pro>
2016-12-13 16:45:15 +0000

    Configuring libidn to use build-machine gtk-doc
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@593 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/GNU/libidn/create-1.28-gtkdoc-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=1.28
+
+tar --files-from=file.list -xzvf ../libidn-$VERSION.tar.gz
+mv libidn-$VERSION libidn-$VERSION-orig
+
+cp -rf ./libidn-$VERSION-new ./libidn-$VERSION
+
+diff -b --unified -Nr  libidn-$VERSION-orig  libidn-$VERSION > libidn-$VERSION-gtkdoc.patch
+
+mv libidn-$VERSION-gtkdoc.patch ../patches
+
+rm -rf ./libidn-$VERSION
+rm -rf ./libidn-$VERSION-orig

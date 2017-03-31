2f67aa932c5c332e64fed5e740b25661be8d4a06
Andrey V.Kosteltsev <kx@radix.pro>
2017-03-31 21:13:48 +0000

    Changes related the new version of development system:   libid3tag, guils, libcap, gettext-tools, eudev.
    Next version of MariaDB 10.0.30, Judy 1.0.5
    
    
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@642 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ sources/packages/l/judy/create-1.0.5-build-cc-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=1.0.5
+
+tar --files-from=file.list -xzvf ../judy-$VERSION.tar.gz
+mv judy-$VERSION judy-$VERSION-orig
+
+cp -rf ./judy-$VERSION-new ./judy-$VERSION
+
+diff -b --unified -Nr  judy-$VERSION-orig  judy-$VERSION > judy-$VERSION-build-cc.patch
+
+mv judy-$VERSION-build-cc.patch ../patches
+
+rm -rf ./judy-$VERSION
+rm -rf ./judy-$VERSION-orig

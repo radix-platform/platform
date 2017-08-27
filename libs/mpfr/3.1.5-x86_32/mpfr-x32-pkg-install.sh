89653f9749820da00b5803c9c6697fceef194029
Andrey V.Kosteltsev <kx@radix.pro>
2017-08-27 22:15:17 +0000

    binutils-2.28, gcc-5.4.0, glibc-2.25
    git-svn-id: https://svn.radix.pro/svn/platform/branches/radix-1.1@654 0a1960e2-aef6-4eae-af04-ff5de3100f3e

---
+++ libs/mpfr/3.1.5-x86_32/mpfr-x32-pkg-install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# arg 1:  the new package version
+pre_install() {
+  /bin/true
+}
+
+# arg 1:  the new package version
+post_install() {
+  /bin/true
+}
+
+# arg 1:  the new package version
+# arg 2:  the old package version
+pre_update() {
+  /bin/true
+}
+
+# arg 1:  the new package version
+# arg 2:  the old package version
+post_update() {
+  post_install
+}
+
+# arg 1:  the old package version
+pre_remove() {
+  /bin/true
+}
+
+# arg 1:  the old package version
+post_remove() {
+  /bin/true
+}
+
+
+operation=$1
+shift
+
+$operation $*

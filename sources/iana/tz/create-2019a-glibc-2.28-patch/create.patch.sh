fd28c41a650a64c6e11ca0719d963a0d14d11c7e
kx <kx@radix.pro>
2019-09-18 16:59:55 +0300

    GNU Libc 2.28; IANA zoneinfo 2019c

---
+++ sources/iana/tz/create-2019a-glibc-2.28-patch/create.patch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+VERSION=2019a
+GLIBC_VERSION=2.28
+
+mkdir -p glibc-$GLIBC_VERSION/timezone
+tar -C glibc-$GLIBC_VERSION/timezone --files-from=file.list -xzvf ../tzcode$VERSION.tar.gz
+mv glibc-$GLIBC_VERSION glibc-$GLIBC_VERSION-orig
+
+cp -rf ./glibc-$GLIBC_VERSION-new ./glibc-$GLIBC_VERSION
+
+diff -b --unified -Nr  glibc-$GLIBC_VERSION-orig  glibc-$GLIBC_VERSION > glibc-$GLIBC_VERSION-tzcode-$VERSION.patch
+
+mv glibc-$GLIBC_VERSION-tzcode-$VERSION.patch ../patches
+
+rm -rf ./glibc-$GLIBC_VERSION
+rm -rf ./glibc-$GLIBC_VERSION-orig
---
+++ sources/iana/tz/create-2019c-version-patch/create.patch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+VERSION=2019c
+
+mkdir -p timezone
+tar -C timezone --files-from=file.list -xzvf ../tzcode$VERSION.tar.gz
+mv timezone timezone-orig
+
+cp -rf ./timezone-new ./timezone
+
+diff -b --unified -Nr  timezone-orig  timezone > tzcode-$VERSION-version.patch
+
+mv tzcode-$VERSION-version.patch ../patches
+
+rm -rf ./timezone
+rm -rf ./timezone-orig

5a70e86a02ae0d131f833e6814bb4495d88c3fb0
kx <kx@radix.pro>
2019-09-24 02:20:22 +0300

    All stuff needed for pcre-8.38

---
+++ sources/packages/l/libunwind/create-1.3.1-ppc64-libdir-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=1.3.1
+
+tar --files-from=file.list -xzvf ../libunwind-$VERSION.tar.gz
+mv libunwind-$VERSION libunwind-$VERSION-orig
+
+cp -rf ./libunwind-$VERSION-new ./libunwind-$VERSION
+
+diff -b --unified -Nr  libunwind-$VERSION-orig  libunwind-$VERSION > libunwind-$VERSION-ppc64-libdir.patch
+
+mv libunwind-$VERSION-ppc64-libdir.patch ../patches
+
+rm -rf ./libunwind-$VERSION
+rm -rf ./libunwind-$VERSION-orig
---
+++ sources/packages/l/libunwind/create-1.3.1-sparc64-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=1.3.1
+
+tar --files-from=file.list -xzvf ../libunwind-$VERSION.tar.gz
+mv libunwind-$VERSION libunwind-$VERSION-orig
+
+cp -rf ./libunwind-$VERSION-new ./libunwind-$VERSION
+
+diff -b --unified -Nr  libunwind-$VERSION-orig  libunwind-$VERSION > libunwind-$VERSION-sparc64.patch
+
+mv libunwind-$VERSION-sparc64.patch ../patches
+
+rm -rf ./libunwind-$VERSION
+rm -rf ./libunwind-$VERSION-orig

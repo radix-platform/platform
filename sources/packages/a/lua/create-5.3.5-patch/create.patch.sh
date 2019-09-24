4924112e58396ab262e6c56d1d4a695b803f163c
kx <kx@radix.pro>
2019-09-24 12:53:33 +0300

    bison-3.4.2, lua-5.3.5

---
+++ sources/packages/a/lua/create-5.3.5-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=5.3.5
+
+tar --files-from=file.list -xzvf ../lua-$VERSION.tar.gz
+mv lua-$VERSION lua-$VERSION-orig
+
+cp -rf ./lua-$VERSION-new ./lua-$VERSION
+
+diff -b --unified -Nr  lua-$VERSION-orig  lua-$VERSION > lua-$VERSION.patch
+
+mv lua-$VERSION.patch ../patches
+
+rm -rf ./lua-$VERSION
+rm -rf ./lua-$VERSION-orig

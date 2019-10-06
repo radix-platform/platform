7e39247177f7651268ea24aea2eb2cde34f7073a
kx <kx@radix.pro>
2019-10-06 22:02:56 +0300

    Python 2.7.16

---
+++ sources/packages/d/python2/create-2.7.16-x86_32-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=2.7.16
+
+tar --files-from=file.list -xJvf ../Python-$VERSION.tar.xz
+mv Python-$VERSION Python-$VERSION-orig
+
+cp -rf ./Python-$VERSION-new ./Python-$VERSION
+
+diff -b --unified -Nr  Python-$VERSION-orig  Python-$VERSION > Python-$VERSION-x86_32.patch
+
+mv Python-$VERSION-x86_32.patch ../patches
+
+rm -rf ./Python-$VERSION
+rm -rf ./Python-$VERSION-orig

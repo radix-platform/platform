164169a1732a8c2a4f67e2e36e9398d25f373429
kx <kx@radix.pro>
2019-10-13 21:37:22 +0300

    libxml2-2.9.9

--- sources/packages/l/libxml2/create-2.9.9-python-patch/create.patch.sh
+++ sources/packages/l/libxml2/create-2.9.9-python-patch/create.patch.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-VERSION=2.9.3
+VERSION=2.9.9
 
 tar --files-from=file.list -xzvf ../libxml2-$VERSION.tar.gz
 mv libxml2-$VERSION libxml2-$VERSION-orig
 
 cp -rf ./libxml2-$VERSION-new ./libxml2-$VERSION
 
-diff -b --unified -Nr  libxml2-$VERSION-orig  libxml2-$VERSION > libxml2-$VERSION.patch
+diff -b --unified -Nr  libxml2-$VERSION-orig  libxml2-$VERSION > libxml2-$VERSION-python.patch
 
-mv libxml2-$VERSION.patch ../patches
+mv libxml2-$VERSION-python.patch ../patches
 
 rm -rf ./libxml2-$VERSION
 rm -rf ./libxml2-$VERSION-orig

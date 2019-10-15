f148d66e1f11765b4e5330e30b484281622458cb
kx <kx@radix.pro>
2019-10-15 21:06:43 +0300

    libxslt-1.1.33

--- sources/packages/l/libxslt/create-1.1.33-python-patch/create.patch.sh
+++ sources/packages/l/libxslt/create-1.1.33-python-patch/create.patch.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-VERSION=1.1.28
+VERSION=1.1.33
 
 tar --files-from=file.list -xzvf ../libxslt-$VERSION.tar.gz
 mv libxslt-$VERSION libxslt-$VERSION-orig
 
 cp -rf ./libxslt-$VERSION-new ./libxslt-$VERSION
 
-diff -b --unified -Nr  libxslt-$VERSION-orig  libxslt-$VERSION > libxslt-$VERSION.patch
+diff -b --unified -Nr  libxslt-$VERSION-orig  libxslt-$VERSION > libxslt-$VERSION-python.patch
 
-mv libxslt-$VERSION.patch ../patches
+mv libxslt-$VERSION-python.patch ../patches
 
 rm -rf ./libxslt-$VERSION
 rm -rf ./libxslt-$VERSION-orig

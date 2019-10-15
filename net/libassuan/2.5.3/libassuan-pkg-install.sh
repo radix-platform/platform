2d843639e0984cacb2e8bda9bee18f169f61e124
kx <kx@radix.pro>
2019-10-15 07:45:40 +0300

    libassuan-2.5.3

--- net/libassuan/2.5.3/libassuan-pkg-install.sh
+++ net/libassuan/2.5.3/libassuan-pkg-install.sh
@@ -29,7 +29,7 @@ post_install() {
   #
   if [ -x /usr/bin/install-info ] ; then
     install-info --info-dir=usr/share/info usr/share/info/assuan.info.gz 2>/dev/null
-  elif ! grep "assuan" usr/share/info/dir 1> /dev/null 2> /dev/null ; then
+  elif ! grep "(assuan)" usr/share/info/dir 1> /dev/null 2> /dev/null ; then
   cat << EOF >> usr/share/info/dir
 
 GNU Libraries

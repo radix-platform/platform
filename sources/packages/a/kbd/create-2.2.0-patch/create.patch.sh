b462aec48da5a451f82585e29da56371dde07a20
kx <kx@radix.pro>
2019-10-24 22:47:58 +0300

    kbd-2.2.0

---
+++ sources/packages/a/kbd/create-2.2.0-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=2.2.0
+
+tar --files-from=file.list -xJvf ../kbd-$VERSION.tar.xz
+mv kbd-$VERSION kbd-$VERSION-orig
+
+cp -rf ./kbd-$VERSION-new ./kbd-$VERSION
+
+diff -b --unified -Nr  kbd-$VERSION-orig  kbd-$VERSION > kbd-$VERSION.patch
+
+mv kbd-$VERSION.patch ../patches
+
+rm -rf ./kbd-$VERSION
+rm -rf ./kbd-$VERSION-orig

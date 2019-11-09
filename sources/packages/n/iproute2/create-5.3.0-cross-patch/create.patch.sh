99e6b447d63758fe0bc4f31c3c7996c45e7ae6ed
kx <kx@radix.pro>
2019-11-09 16:42:32 +0300

    iptables-1.8.3, iproute2-5.3.0

--- sources/packages/n/iproute2/create-5.3.0-cross-patch/create.patch.sh
+++ sources/packages/n/iproute2/create-5.3.0-cross-patch/create.patch.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-VERSION=3.16.0
+VERSION=5.3.0
 
 tar --files-from=file.list -xJvf ../iproute2-$VERSION.tar.xz
 mv iproute2-$VERSION iproute2-$VERSION-orig
 
 cp -rf ./iproute2-$VERSION-new ./iproute2-$VERSION
 
-diff -b --unified -Nr  iproute2-$VERSION-orig  iproute2-$VERSION > iproute2-$VERSION.patch
+diff -b --unified -Nr  iproute2-$VERSION-orig  iproute2-$VERSION > iproute2-$VERSION-cross.patch
 
-mv iproute2-$VERSION.patch ../patches
+mv iproute2-$VERSION-cross.patch ../patches
 
 rm -rf ./iproute2-$VERSION
 rm -rf ./iproute2-$VERSION-orig

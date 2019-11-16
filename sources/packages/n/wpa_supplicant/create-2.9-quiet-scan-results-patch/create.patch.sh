c8f36500c4a2f9cd443b8e7d9289f23321a2a700
kx <kx@radix.pro>
2019-11-16 21:39:42 +0300

    wpa_supplicant-2.9

---
+++ sources/packages/n/wpa_supplicant/create-2.9-quiet-scan-results-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=2.9
+
+tar --files-from=file.list -xzvf ../wpa_supplicant-$VERSION.tar.gz
+mv wpa_supplicant-$VERSION wpa_supplicant-$VERSION-orig
+
+cp -rf ./wpa_supplicant-$VERSION-new ./wpa_supplicant-$VERSION
+
+diff -b --unified -Nr  wpa_supplicant-$VERSION-orig  wpa_supplicant-$VERSION > wpa_supplicant-$VERSION-quiet-scan-results.patch
+
+mv wpa_supplicant-$VERSION-quiet-scan-results.patch ../patches
+
+rm -rf ./wpa_supplicant-$VERSION
+rm -rf ./wpa_supplicant-$VERSION-orig
